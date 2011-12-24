a907e3b46aac83a55148576ade48b01cb900e4e9
Colin Campbell-McPherson <colin@logaan.net>
2011-12-25 00:50:43 +1100

    more refactoring

--- interface.erl
+++ interface.erl
@@ -4,46 +4,52 @@
 -include_lib("eunit/include/eunit.hrl").
 
 start() ->
-  setup(),
-  start_game().
+  setup_curses(),
 
-start_game() ->
   StartingPosition = {0, 0},
   print_string(StartingPosition, "@"),
   cecho:refresh(),
-  loop(StartingPosition).
 
-loop(OldPosition) ->
-  case cecho:getch() of
-    $k -> redraw_player(OldPosition, move_up(OldPosition));
-    $j -> redraw_player(OldPosition, move_down(OldPosition));
-    $h -> redraw_player(OldPosition, move_left(OldPosition));
-    $l -> redraw_player(OldPosition, move_right(OldPosition));
-    $ -> teardown();
-    _ -> loop(OldPosition)
-  end.
+  handle_input(StartingPosition).
 
-move_up({X, Y})    -> {X, Y-1}.
-move_down({X, Y})  -> {X, Y+1}.
-move_left({X, Y})  -> {X-1, Y}.
-move_right({X, Y}) -> {X+1, Y}.
-
-redraw_player(OldPosition, NewPosition) ->
-  print_string(OldPosition, " "),
-  print_string(NewPosition, "@"),
-  cecho:refresh(),
-  loop(NewPosition).
-
-print_string({X, Y}, String) ->
-  cecho:mvaddstr(Y, X, String).
-
-setup() ->
+setup_curses() ->
   application:start(cecho),
   cecho:cbreak(),
   cecho:noecho(),
   cecho:curs_set(?ceCURS_INVISIBLE).
 
-teardown() ->
+teardown_curses() ->
   application:stop(cecho),
   erlang:halt().
+
+handle_input(OldPosition) ->
+  case cecho:getch() of
+    $k -> redraw_player(OldPosition, move_up(OldPosition));
+    $j -> redraw_player(OldPosition, move_down(OldPosition));
+    $h -> redraw_player(OldPosition, move_left(OldPosition));
+    $l -> redraw_player(OldPosition, move_right(OldPosition));
+    $ -> teardown_curses();
+    _ -> handle_input(OldPosition)
+  end.
+
+redraw_player(OldPosition, NewPosition) ->
+  print_string(OldPosition, " "),
+  print_string(NewPosition, "@"),
+  cecho:refresh(),
+  handle_input(NewPosition).
+
+print_string({X, Y}, String) ->
+  cecho:mvaddstr(Y, X, String).
+
+move_up({X, Y}) when Y > 0 -> {X, Y-1};
+move_up(Position) -> Position.
+
+move_down({X, Y}) when Y < 23 -> {X, Y+1};
+move_down(Position) -> Position.
+
+move_left({X, Y}) when X > 0 -> {X-1, Y};
+move_left(Position) -> Position.
+
+move_right({X, Y}) when X < 79 -> {X+1, Y};
+move_right(Position) -> Position.
 
