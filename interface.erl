091996c07c5a07054990127fabc3577d78d66c3f
Colin Campbell-McPherson <colin@logaan.net>
2011-12-25 23:59:19 +1100

    made move_x and move_y tests clearer with unit tests and macros

--- interface.erl
+++ interface.erl
@@ -3,6 +3,11 @@
 -include_lib("../cecho/include/cecho.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
+-define(up, $k).
+-define(down, $j).
+-define(left, $h).
+-define(right, $l).
+
 start() ->
   setup_curses(),
   game_loop({{0, 0}, [{2, 0, "D"},
@@ -11,7 +16,6 @@ start() ->
                       {2, 6, "G"},
                       {2, 8, "M"}]}).
 
-% TODO: Turn world into a record
 game_loop(World) ->
   redraw_world(World),
   case cecho:getch() of
@@ -26,16 +30,16 @@ move({OldPosition = {X, Y}, NPCs}, Direction) ->
     false -> {NewPosition, NPCs}
   end.
 
-move_y(Y, $k) when Y > 0  -> Y - 1;
-move_y(Y, $j) when Y < 23 -> Y + 1;
+move_y(Y, ?up)   when Y > 0  -> Y - 1;
+move_y(Y, ?down) when Y < 23 -> Y + 1;
 move_y(Y, _) -> Y.
 
-move_x(X, $h) when X > 0  -> X - 1;
-move_x(X, $l) when X < 79 -> X + 1;
+move_x(X, ?left)  when X > 0  -> X - 1;
+move_x(X, ?right) when X < 79 -> X + 1;
 move_x(X, _) -> X.
 
-colision(_, []) -> false;
-colision({X, Y}, [{X, Y, _} | _]) -> true;
+colision(_Player, []) -> false;
+colision({X, Y}, [{X, Y, _} | _NPCs]) -> true;
 colision(Player, [_ | NPCs]) -> colision(Player, NPCs).
 
 % IOey things. yuck.
@@ -63,4 +67,24 @@ draw_npcs([]) -> ok;
 draw_npcs([{X, Y, Char} | NPCs]) ->
   cecho:mvaddstr(Y, X, Char),
   draw_npcs(NPCs).
+
+% Tests
+
+move_x_test() ->
+  1 = move_x(2, ?left),
+  0 = move_x(1, ?left),
+  0 = move_x(0, ?left),
+
+  78 = move_x(77, ?right),
+  79 = move_x(78, ?right),
+  79 = move_x(79, ?right).
+
+move_y_test() ->
+  1 = move_y(2, ?up),
+  0 = move_y(1, ?up),
+  0 = move_y(0, ?up),
+
+  22 = move_y(21, ?down),
+  23 = move_y(22, ?down),
+  23 = move_y(23, ?down).
 
