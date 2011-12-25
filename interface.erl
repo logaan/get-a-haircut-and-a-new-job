00ad6055f41d846d3ddbcda756d31937880f79ed
Colin Campbell-McPherson <colin@logaan.net>
2011-12-26 00:19:49 +1100

    tidied up movement/colision code and change up/down to north/south

--- interface.erl
+++ interface.erl
@@ -3,10 +3,11 @@
 -include_lib("../cecho/include/cecho.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
--define(up, $k).
--define(down, $j).
--define(left, $h).
--define(right, $l).
+-define(quit,  $).
+-define(north, $k).
+-define(south, $j).
+-define(west,  $h).
+-define(east,  $l).
 
 start() ->
   setup_curses(),
@@ -19,30 +20,9 @@ start() ->
 game_loop(World) ->
   redraw_world(World),
   case cecho:getch() of
-    $ -> teardown_curses();
+    ?quit -> teardown_curses();
     Direction -> game_loop(move(World, Direction))
   end.
-
-move({OldPosition = {X, Y}, NPCs}, Direction) ->
-  NewPosition = {move_x(X, Direction), move_y(Y, Direction)},
-  case colision(NewPosition, NPCs) of
-    true -> {OldPosition, NPCs};
-    false -> {NewPosition, NPCs}
-  end.
-
-move_y(Y, ?up)   when Y > 0  -> Y - 1;
-move_y(Y, ?down) when Y < 23 -> Y + 1;
-move_y(Y, _) -> Y.
-
-move_x(X, ?left)  when X > 0  -> X - 1;
-move_x(X, ?right) when X < 79 -> X + 1;
-move_x(X, _) -> X.
-
-colision(_Player, []) -> false;
-colision({X, Y}, [{X, Y, _} | _NPCs]) -> true;
-colision(Player, [_ | NPCs]) -> colision(Player, NPCs).
-
-% IOey things. yuck.
 
 setup_curses() ->
   application:start(cecho),
@@ -68,23 +48,51 @@ draw_npcs([{X, Y, Char} | NPCs]) ->
   cecho:mvaddstr(Y, X, Char),
   draw_npcs(NPCs).
 
+% Utilities
+
+move({OldPosition = {X, Y}, NPCs}, Direction) ->
+  NewPosition = {move_x(X, Direction), move_y(Y, Direction)},
+  {colision(OldPosition, NewPosition, NPCs), NPCs}.
+
+move_y(Y, ?north) when Y > 0  -> Y - 1;
+move_y(Y, ?south) when Y < 23 -> Y + 1;
+move_y(Y, _) -> Y.
+
+move_x(X, ?west) when X > 0  -> X - 1;
+move_x(X, ?east) when X < 79 -> X + 1;
+move_x(X, _) -> X.
+
+colision(_OldPosition, NewPosition, []) ->
+  NewPosition;
+colision(OldPosition, {X, Y}, [{X, Y, _} | _NPCs]) ->
+  OldPosition;
+colision(OldPosition, NewPosition, [_ | NPCs]) ->
+  colision(OldPosition, NewPosition, NPCs).
+
 % Tests
 
-move_x_test() ->
-  1 = move_x(2, ?left),
-  0 = move_x(1, ?left),
-  0 = move_x(0, ?left),
+move_test() ->
+  NPCs = [{2, 2, $T}],
+  {{1, 0}, NPCs} = move({{1, 1}, NPCs}, ?north),
+  {{1, 2}, NPCs} = move({{1, 1}, NPCs}, ?south),
+  {{0, 1}, NPCs} = move({{1, 1}, NPCs}, ?west),
+  {{2, 1}, NPCs} = move({{1, 1}, NPCs}, ?east).
 
-  78 = move_x(77, ?right),
-  79 = move_x(78, ?right),
-  79 = move_x(79, ?right).
+move_x_test() ->
+  1 = move_x(2, ?west),
+  0 = move_x(1, ?west),
+  0 = move_x(0, ?west),
+
+  78 = move_x(77, ?east),
+  79 = move_x(78, ?east),
+  79 = move_x(79, ?east).
 
 move_y_test() ->
-  1 = move_y(2, ?up),
-  0 = move_y(1, ?up),
-  0 = move_y(0, ?up),
+  1 = move_y(2, ?north),
+  0 = move_y(1, ?north),
+  0 = move_y(0, ?north),
 
-  22 = move_y(21, ?down),
-  23 = move_y(22, ?down),
-  23 = move_y(23, ?down).
+  22 = move_y(21, ?south),
+  23 = move_y(22, ?south),
+  23 = move_y(23, ?south).
 
